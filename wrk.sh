b044c59607fb814946fa5bbb04769bde27530e64
杨尚山 <cn.yangss@gmail.com>
2022-02-05 11:43:16 +0800

    wrk2

--- wrk.sh
+++ wrk.sh
@@ -1,1 +1,3 @@
-wrk -t 4 -c 100 -d 10s --latency "http://localhost:8080/UTC/now"
+# wrk2 --version
+# wrk 4.0.0 [kqueue] Copyright (C) 2012 Will Glozer
+wrk2 -t 4 -c 100 -d 10s -R100 --latency "http://localhost:8080/UTC/now"
